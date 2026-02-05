8d53ef16f48e5c8e168a3016ea7b02a4e40a3105
Murtaza Kalangwala <murtazakalangwala@Murtazas-MacBook-Pro.local>
2026-02-05 19:54:33 +0530

    Revert chat icon form whatsapp in vendor

--- yofrezco-6ammart-laravel-admin/resources/views/layouts/vendor/partials/_sidebar.blade.php
+++ yofrezco-6ammart-laravel-admin/resources/views/layouts/vendor/partials/_sidebar.blade.php
@@ -780,8 +780,7 @@
                         <li class="navbar-vertical-aside-has-menu">
                             <a class="js-navbar-vertical-aside-menu-link nav-link" href="https://wa.link/yofrezco"
                                 target="_blank" title="{{ translate('messages.whatsapp') }}">
-                                <img src="{{asset('public/assets/admin/img/whatsapp.png')}}" alt="WhatsApp" class="nav-icon"
-                                    width="18">
+                                <i class="tio-chat nav-icon"></i>
                                 <span class="navbar-vertical-aside-mini-mode-hidden-elements text-truncate">
                                     {{ translate('messages.Whatsapp') }}
                                 </span>
